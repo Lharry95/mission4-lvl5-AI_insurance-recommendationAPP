5c7bf115a54470e1c9117e5460b83c0dd899aa78
Lharyzza <lharyzzav@missionreadyhq.com>
2026-03-10 09:22:03 +1300

    updated css for input box & submit button - onclick button function working on console

--- client/src/components/InputBox.tsx
+++ client/src/components/InputBox.tsx
@@ -1,7 +1,26 @@
 import styles from "./InputBox.module.css";
+import { type MouseEvent } from "react";
 
 function InputBox() {
-  return <div className={styles.inputBoxContainer}>InputBox</div>;
+  const submitResponse = (event: MouseEvent<HTMLButtonElement>) => {
+    console.log("Button was clicked!", event.clientX);
+  };
+
+  return (
+    <div className={styles.inputBoxContainer}>
+      <div className={styles.inputResponse}>
+        <input
+          className={styles.inputBox}
+          type="text"
+          id="car Chat Box"
+          placeholder="Enter Text here.."
+        ></input>
+      </div>
+      <button className={styles.inputButton} onClick={submitResponse}>
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export default InputBox;
